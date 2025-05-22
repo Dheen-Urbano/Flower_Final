08dcb1b6ed9c42edd5888f7f1c490fbe6cb58a1c
Dheen-Urbano <qdburbano@tip.edu.ph>
2025-05-22 10:20:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import load_model
 import urllib.request
 import os
 
-# Download model if it doesn't exist
+
 MODEL_URL = "https://github.com/Dheen-Urbano/Flower_Final/blob/main/flower_model.h5" 
 MODEL_PATH = "flower_model.h5"
 
@@ -25,22 +25,22 @@ st.write("Upload an FLower image to classify.")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Read image file
+
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
-    # Show original image
+
     st.image(image, caption='Uploaded Image', channels="BGR")
 
-    # Resize to expected input size
-    expected_size = model.input_shape[1:3]  # (height, width)
+
+    expected_size = model.input_shape[1:3]  
     resized_image = cv2.resize(image, expected_size)
 
-    # Convert to RGB and normalize
-    rgb_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
-    input_image = np.expand_dims(rgb_image / 255.0, axis=0)  # Shape: (1, H, W, 3)
 
-    # Predict
+    rgb_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
+    input_image = np.expand_dims(rgb_image / 255.0, axis=0)  
+
+
     prediction = model.predict(input_image)
     predicted_class = class_names[np.argmax(prediction)]
 
